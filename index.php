af6f2c1e9f7374b2497a19db61b80d0376374217
Rajdeep Ratan <rajdeepratan@hotmail.com>
2022-02-07 14:51:50 -0500

    Home Page And Footer

--- index.php
+++ index.php
@@ -7,12 +7,24 @@
 
 
 <?php
-    $myarray = array(FILE_IMAGE,FILE_IMAGE1,FILE_IMAGE2);
+    $myarray = array(COLLOCATION1,COLLOCATION2,COLLOCATION3,COLLOCATION4,COLLOCATION5);
     shuffle($myarray);
 ?>
-
-    <img src="<?php echo $myarray[0]; ?>" class ="advertising" alt="advertising">
-
+    <div class="home-page">
+        <div class="mt-5 d-flex justify-content-between align-items-center">
+            <div class="me-3">
+                <a href="#">
+                    <img class="<?php echo $myarray[0] =='assets/images/collocation-1.jpeg'? "red-border" : "" ?>" alt="advertising" src="<?php echo $myarray[0]; ?>">
+                </a>
+            </div>
+            <div class="text-center">
+                <h2>Style Yard</h2>
+                <p>
+                    Style Yard, India’s no. 1 online fashion destination justifies its fashion relevance by bringing something new and chic to the table on the daily. Fashion trends seem to change at lightning speed, yet the Style Yard shopping app has managed to keep up without any hiccups. In addition, Style Yard has vowed to serve customers to the best of its ability by introducing its first-ever loyalty program, The Style Yard Insider. Gain access to priority delivery, early sales, lucrative deals and other special perks on all your shopping with the Style Yard app. Download the Style Yard app on your Android or IOS device today and experience shopping like never before!
+                </p>
+            </div>
+        </div>
+    </div>
 
 <?php
 
--- phpFunction/functionProducts.php
+++ phpFunction/functionProducts.php
@@ -9,8 +9,17 @@ define("FOLDER_CSS", "./CSS/");
 define("FILE_CSS", FOLDER_CSS . "style.css");
 define("FOLDER_PICTURES", "assets/images/");
 define("WEBSITE_LOGO", FOLDER_PICTURES . "logo.png");
-define("FILE_IMAGE1", FOLDER_PICTURES . "select2.JPG");
-define("FILE_IMAGE2", FOLDER_PICTURES . "select3.JPG");
+define("COLLOCATION1", FOLDER_PICTURES . "collocation-1.jpeg");
+define("COLLOCATION2", FOLDER_PICTURES . "collocation-2.jpeg");
+define("COLLOCATION3", FOLDER_PICTURES . "collocation-3.jpeg");
+define("COLLOCATION4", FOLDER_PICTURES . "collocation-4.jpeg");
+define("COLLOCATION5", FOLDER_PICTURES . "collocation-5.jpeg");
+define("COLLOCATION6", FOLDER_PICTURES . "collocation-6.jpeg");
+define("COLLOCATION7", FOLDER_PICTURES . "collocation-7.jpeg");
+define("COLLOCATION8", FOLDER_PICTURES . "collocation-8.jpeg");
+define("COLLOCATION9", FOLDER_PICTURES . "collocation-9.jpeg");
+
+
 
 function pageTop($pageTitle) {
     ?>
@@ -23,6 +32,8 @@ function pageTop($pageTitle) {
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+
             <title><?php echo $pageTitle; ?></title>
         </head>
         <body>
@@ -36,39 +47,51 @@ function navigationMenu() {
             <div class="logo-section">
                 <img class="logo" src="<?php echo WEBSITE_LOGO ?>" alt="website-logo"/>
             </div>
-
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="<?php echo FILE_INDEX ?>">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="<?php echo FILE_PRODUCTS ?>">Products</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="<?php echo FILE_CONTACT ?>">Contact Us</a>
-                        </li>
-                    </ul>
+            <div id="topheader">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="<?php echo FILE_INDEX ?>">Home</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="<?php echo FILE_PRODUCTS ?>">Products</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="<?php echo FILE_CONTACT ?>">Contact Us</a>
+                            </li>
+                        </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         </div>
+        <div class="container">
     <?php
 }
 
 function pageBottom() {
-    ?>
-                <footer>
-                    <h3 class="copy-right">Copyright Rajdeep Singh Ratan (2110167) 2022</h3>
+    ?>      </div>
+                <footer class="mt-4">
+                    <p class="copy-right">Copyright <span>Rajdeep Singh Ratan (2110167)</span> <?php echo date("Y"); ?></p>
                 </footer>
+
+                <script>
+                    $( '#topheader .navbar-nav a' ).on('click', 
+                                function () {
+                        $( '#topheader .navbar-nav' ).find( 'a.active' )
+                        .removeClass( 'active' );
+                        $( this ).parent( 'a' ).addClass( 'active' );
+                    });
+                </script>
+
             </body>
         </html>
+        
     <?php
 }
 
