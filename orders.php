fe9410e58dc3fb5709024ca3e7cd1b240438c983
Rajdeep Ratan <rajdeepratan@hotmail.com>
2022-02-22 12:53:03 -0500

    Order Page GET params

--- orders.php
+++ orders.php
@@ -2,8 +2,7 @@
 
     include_once "./phpFunction/functionProducts.php";
     pageTop("Orders");
-
-    #echo $_POST['command'];
+    
 ?>
 
     <div class="order-page pt-5 pb-5">
@@ -25,23 +24,6 @@
             </thead>
             <tbody>
                 <?php showTableData(); ?>
-                <!-- <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                </tr> -->
             </tbody>
         </table>
 
--- phpFunction/functionProducts.php
+++ phpFunction/functionProducts.php
@@ -4,6 +4,7 @@ define("DEBUGGING_MODE", true);
 define("FOLDER_ERROR", "./errors/");
 define("FILE_ERROR", FOLDER_ERROR ."log.txt");
 $currentDateTime = date('Y-m-d');
+$curPageName = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
 
 function manageError($errorNumber, $errorString, $errorFile, $errorLine) {
     global $currentDateTime;
@@ -78,11 +79,17 @@ define("DATA_FOLDER", "./dataFile/");
 define("DATA_FILE", DATA_FOLDER . "data.txt");
 
 
+// Page Top Section
 function pageTop($pageTitle) {
+
+    global $curPageName;
+
+
     header('Expires: Sat, 03 Dec 1994 16:00:00 GMT');
     header('CacheControl: no-cache');
     header('Pragma: no-cache');
     header('Content-type: text/html; charset=UTF-8');
+
     ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
         <html>
         <head>
@@ -91,18 +98,29 @@ function pageTop($pageTitle) {
             
             <link rel="stylesheet" href="<?php echo FILE_CSS ?>"/>
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-            <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+            <!-- Bootstrap CSS -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 
             <title><?php echo $pageTitle; ?></title>
         </head>
-        <body>
+        <body class="
+            <?php 
+                if($curPageName === FILE_ORDERS && isset($_GET["command"])) {
+                    if ($_GET["command"] == "print") {
+                        echo "print-screen";
+                    }
+                }
+            ?>
+        ">
+            
     <?php
     navigationMenu();
 }
 
+// Page Menu
 function navigationMenu() {
+    global $curPageName;
     ?>  
         <div class="nav-bar">
             <div class="logo-section">
@@ -117,13 +135,38 @@ function navigationMenu() {
                         <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="<?php echo FILE_INDEX ?>">Home</a>
+                                <a 
+                                    class="nav-link 
+                                        <?php if($curPageName === FILE_INDEX)
+                                            echo "active";
+                                        ?>"
+                                    aria-current="page" 
+                                    href="<?php echo FILE_INDEX ?>"
+                                >
+                                    Home
+                                </a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="<?php echo FILE_PRODUCTS ?>">Products</a>
+                                <a 
+                                    class="nav-link 
+                                        <?php if($curPageName === FILE_PRODUCTS)
+                                            echo "active";
+                                        ?>"
+                                    href="<?php echo FILE_PRODUCTS ?>"
+                                >
+                                    Products
+                                </a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="<?php echo FILE_ORDERS ?>">Orders</a>
+                                <a
+                                    class="nav-link 
+                                        <?php if($curPageName === FILE_ORDERS)
+                                            echo "active";
+                                        ?>"
+                                    href="<?php echo FILE_ORDERS ?>"
+                                >
+                                    Orders
+                                </a>
                             </li>
                         </ul>
                         </div>
@@ -135,17 +178,31 @@ function navigationMenu() {
     <?php
 }
 
+// Page Footer Section
 function pageBottom() {
     ?>      </div>
                 <footer class="">
                     <p class="copy-right">Copyright <span>Rajdeep Singh Ratan (2110167)</span> <?php echo date("Y"); ?></p>
                 </footer>
+
+                <!-- Optional JavaScript; choose one of the two! -->
+
+                <!-- Option 1: Bootstrap Bundle with Popper -->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+                <!-- Option 2: Separate Popper and Bootstrap JS -->
+                <!--
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+                -->
             </body>
         </html>
         
     <?php
 }
 
+
+// Order Page Show Table Data Section
 function showTableData(){
     if(file_exists(DATA_FILE)){
 
@@ -166,7 +223,19 @@ function showTableData(){
                     if($key == 0) {
                         echo "<th scope='row'>".$value."</th>";
                     } else if ($key == 5 || $key == 7 || $key == 8 ||$key == 9) {
-                        echo "<td>".$value."$</td>";
+                        echo "<td class=";
+                        if($key == 7 && isset($_GET["command"])) {
+                            if ($_GET["command"] == "color") {
+                                if($value < 100.00) {
+                                    echo "red-text";
+                                } else if ($value < 999.99) {
+                                    echo "orange-text";
+                                } else {
+                                    echo "green-text";
+                                }
+                            }
+                        }
+                        echo ">".$value."$</td>";
                     } else {
                         echo "<td>".$value."</td>";
                     }   
@@ -180,5 +249,3 @@ function showTableData(){
         fclose($fileHandle);
     }
 }
-
-
