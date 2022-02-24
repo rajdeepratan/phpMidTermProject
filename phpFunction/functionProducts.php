27425229e47f9eb7f9b28c5c1c3ee3b3ef66762b
Rajdeep Ratan <rajdeepratan@hotmail.com>
2022-02-24 13:14:09 -0500

    Remove commented code

--- phpFunction/functionProducts.php
+++ phpFunction/functionProducts.php
@@ -191,16 +191,8 @@ function pageBottom() {
                     <p class="copy-right">Copyright <span>Rajdeep Singh Ratan (2110167)</span> <?php echo date("Y"); ?></p>
                 </footer>
 
-                <!-- Optional JavaScript; choose one of the two! -->
-
                 <!-- Option 1: Bootstrap Bundle with Popper -->
                 <script type="text/javascript" src="<?php echo BOOTSTRAP_JS ?>"></script>
-
-                <!-- Option 2: Separate Popper and Bootstrap JS -->
-                <!--
-                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-                -->
             </body>
         </html>
         
