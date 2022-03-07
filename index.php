2696347a93bcf71e03cbc057a17259c8119b0aa0
Rajdeep Ratan <rajdeepratan@hotmail.com>
2022-03-06 19:45:22 -0500

    Add link on home page adds image click

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <div class="home-page pt-5 pb-5">
         <div class="d-flex justify-content-between align-items-center">
             <div class="me-3">
-                <a href="#">
+                <a href="https://www.google.ca/" target=_blank>
                     <img class="<?php echo $collocationArray[0] == COLLOCATION1 ? "red-border" : "" ?>" alt="advertising" src="<?php echo $collocationArray[0]; ?>">
                 </a>
             </div>
